0b67976c595c2846d8607fa258cff94a79f67441
lbh2076 <leebh963@gmail.com>
2024-12-04 17:29:14 +0900

    Update github_api.py

--- app/github_api.py
+++ app/github_api.py
@@ -4,4 +4,4 @@ def fetch_repo_files(token, repo_name):
     g = Github(token)
     repo = g.get_repo(repo_name)
     files = repo.get_contents("")
-    return [file.path for file in files if file.type == "file"]
+    return [file.path for file in files if file.type == "file" ]
